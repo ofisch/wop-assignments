99d3094ba0c5ee799577d844ce25a8175d1f0e46
Onni Fischer <onni.fischer@gmail.com>
2022-11-03 09:08:56 +0200

    middleware

---
+++ week2/controllers/userController.js
@@ -0,0 +1,24 @@
+'use strict';
+const {users} = require('../models/userModel');
+const {getUser} = require('../models/userModel');
+
+const user_list_get = (req, res) => {
+  res.json(users);
+};
+
+const user_get = (req, res) => {
+  const user = getUser(req.params.id);
+  console.log('käyttäjä', user);
+  res.json(user);
+};
+
+const user_post = (req, res) => {
+  console.log(req.body);
+  res.send('Add user route');
+}
+
+module.exports = {
+  user_list_get,
+  user_get,
+  user_post,
+};
